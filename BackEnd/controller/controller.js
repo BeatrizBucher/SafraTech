10039566970f247fde2fdf24015eee85069d81ed
Ana Chagas <anacla.032008@gmail.com>
2025-10-03 14:13:00 -0300

    FEAT: iniciando a controller de eventos

--- BackEnd/controller/controller.js
+++ BackEnd/controller/controller.js
@@ -1,9 +1,9 @@
-const HistoricosModel = require('../models/model');
+const Models = require('../models/model');
 
 const HistoricosController = {
-     listaHitoricoCompleto: async (req, res) =>{
+    listaHitoricoCompleto: async (req, res) => {
         try {
-            const histCompleto = await HistoricosModel.listaHitoricoCompleto();
+            const histCompleto = await Models.HistoricosModel.listaHitoricoCompleto();
 
             console.log(histCompleto);
 
@@ -17,23 +17,61 @@ const HistoricosController = {
 
     listaHitorico: async (req, res) => {
         try {
-                const { id } = req.params;
-                if (!id) {
-                    return res.status(400).json({ mensagem: 'ID inválido' });
-                }
-    
-                const listaHistID = await HistoricosModel.listaHitorico(id);
-    
-                if (!listaHistID) {
-                    return res.status(404).json({ mensagem: 'Histórico não encontrado'});  
-                }
-    
-                res.status(200).json(listaHistID);
-            } catch (error) {
-                console.log(error);
-                res.status(500).json({ mensagem: error.message });
+            const { id } = req.params;
+            if (!id) {
+                return res.status(400).json({ mensagem: 'ID inválido' });
             }
+
+            const listaHistID = await HistoricosModel.listaHitorico(id);
+
+            if (!listaHistID) {
+                return res.status(404).json({ mensagem: 'Histórico não encontrado' });
+            }
+
+            res.status(200).json(listaHistID);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ mensagem: error.message });
+        }
     }
 }
 
-module.exports = HistoricosController
+const EventosController = {
+    cadastrarEventos: async (req, res) => {
+        const { informacao, dispositivo_id } = req.body;
+
+        try {
+            if (!informacao || !dispositivo_id) {
+                return res.status(400).json({ mensagem: "O campo da informação é obrigatório" });
+            }
+
+            const novoEvento = await Models.EventosModel.Eventos({
+                informacao,
+                dispositivo_id
+            });
+
+            console.log(novoEvento);
+
+            if (novoEvento.affectedRows > 0) {
+                return res.status(201).json({
+                    success: true,
+                    data: novoEvento
+                });
+            }
+            else {
+                res.status(401).json({ msg: "Falha ao cadastrar um novo evento" })
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                mensagem: error.message
+            });
+        }
+    },
+}
+
+module.exports = {
+    HistoricosController,
+    EventosController
+}
--- BackEnd/src/routers.js
+++ BackEnd/src/routers.js
@@ -2,7 +2,7 @@ const express = require('express')
 const controller = require('../controller/controller')
 const router = express.Router()
 
-router.get('listaHistoricoCompleto', controller.listaHitoricoCompleto)
-router.get('listaHistorico/:id', controller.listaHitorico)
-
+router.get('listaHistoricoCompleto', controller.listaHitoricoCompleto);
+router.get('listaHistorico/:id', controller.listaHitorico);
+router.post('cadastrarEventos', controller.cadastrarEventos);
 module.exports = router;
